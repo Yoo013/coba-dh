a98917cd5dfab58079c5db845400afcd8bdc06a0
Yoo013 <alfraka744@gmail.com>
2024-06-10 13:06:47 +0700

    frontend

--- frontend/src/components/Singup/index.jsx
+++ frontend/src/components/Singup/index.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:3000/api/users";
+      const url = "https://coba-dh-server.vercel.app/api/users";
       const { data: res } = await axios.post(url, data);
       navigate("/login");
 
